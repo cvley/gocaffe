989ee3910b531819db311e798e3e380a14244987
cvley <ley@hackcv.com>
2017-01-03 00:15:28 +0800

    fix proto conflict with origin dir name

--- net.go
+++ net.go
@@ -1,17 +1,37 @@
 package net
 
 import (
-	"github.com/cvley/gocaffe/proto"
+	"github.com/cvley/gocaffe/caffeproto"
+	"github.com/golang/protobuf/proto"
+	"io/ioutil"
 	"log"
+	"os"
 )
 
 type Net struct {
 }
 
-func (net *Net) CopyTrainedLayersFromParam(netParam *proto.NetParameter) (*Net, error) {
+func (net *Net) CopyTrainedLayersFromParam(netParam *caffeproto.NetParameter) (*Net, error) {
 
 }
 
 func (net *Net) CopyTrainedLayersFromFile(file string) (*Net, error) {
-	param := &proto.NetParameter{}
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+
+	param := &caffeproto.NetParameter{}
+	if err := proto.Unmarshal(b, param); err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("%+v\n", param)
+
+	return nil, nil
 }
