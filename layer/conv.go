d9c5751c78ef60f53631574f1b43e6ead778f9e0
cvley <ley@hackcv.com>
2017-01-12 08:20:30 +0800

    init convolution layer

--- layer/conv.go
+++ layer/conv.go
@@ -4,27 +4,52 @@ import (
 	"log"
 
 	pb "github.com/cvley/gocaffe"
+	"github.com/cvley/gocaffe/blob"
+	"github.com/cvley/gocaffe/math"
 )
 
 type ConvLayer struct {
+	ConvParam *pb.ConvolutionParameter
+	IsShared  bool
+	Phase     *pb.Phase
+	blobs     []*blob.Blob
 }
 
-func NewConvolutionLayer(param *pb.LayerParameter) (*ConvLayer, error) {
+func NewConvolutionLayer(param *pb.LayerParameter) *ConvLayer {
+	phase := param.GetPhase()
+	blobprotos := param.GetBlobs()
+	blobs := make([]*blob.Blob, len(blobprotos))
+	for i, p := range blobprotos {
+		blobs[i].FromProto(p, true)
+	}
+	return &ConvLayer{
+		ConvParam: param.GetConvolutionParam(),
+		IsShared:  false,
+		Phase:     phase,
+		blobs:     blobs,
+	}
 }
 
-func (conv *ConvLayer) SetUp(bottom, top []*Blob) {
+func (conv *ConvLayer) SetUp(bottom, top []*blob.Blob) {
 }
 
-func (conv *ConvLayer) Reshape(bottom, top []*Blob) {
+func (conv *ConvLayer) Reshape(bottom, top []*blob.Blob) {
 }
 
-func (conv *ConvLayer) Forward(bottom, top []*Blob) {
+func (conv *ConvLayer) Forward(bottom, top []*blob.Blob) {
 }
 
-func (conv *ConvLayer) Backward(bottom, top []*Blob, propagateDown []bool) {
+func (conv *ConvLayer) Backward(bottom, top []*blob.Blob, propagateDown []bool) {
 	// not implement yet, only forward is enough
 }
 
 func (conv *ConvLayer) Type() string {
 	return "ConvolutionLayer"
+}
+
+func im2colNd() {
+
+}
+
+func im2col() {
 }
