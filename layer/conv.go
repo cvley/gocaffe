90977bae8f74ed6c485c2caede1e479831fa038a
cvley <ley@hackcv.com>
2017-01-12 23:07:39 +0800

    add more in conv.go

--- layer/conv.go
+++ layer/conv.go
@@ -1,8 +1,8 @@
 package layer
 
 import (
-	"log"
 	"errors"
+	"log"
 	"math"
 
 	pb "github.com/cvley/gocaffe"
@@ -18,7 +18,10 @@ type ConvLayer struct {
 
 	forceNdim2col   bool
 	channelAxis     int
-	numSpatialAxis int
+	numSpatialAxis  int
+	kernelShapeData *blob.Blob
+	stride          *blob.Blob
+	pad             *blob.Blob
 }
 
 func NewConvolutionLayer(param *pb.LayerParameter) *ConvLayer {
@@ -54,6 +57,97 @@ func (conv *ConvLayer) SetUp(bottom, top []*blob.Blob) error {
 	}
 
 	// setup filter kernel dimensions (kernel_shape)
+	conv.kernel_shape_data.Reshape(spatialDimBlobShape)
+	if conv.ConvParam.GetKernelH() > 0 || conv.ConvParam.GetKernelW() > 0 {
+		if conv.numSpatialAxis != 2 {
+			return errors.New("kernel_h & kernel_w can only be used for 2D convolution.")
+		}
+		if len(conv.ConvParam.GetKernelSize()) != 0 {
+			return errors.New("Either kernel_size or kernel_h/w should be specified; not both.")
+		}
+		conv.kernelShapeData.Data[0] = conv.ConvParam.GetKernelH()
+		conv.kernelShapeData.Data[1] = conv.ConvParam.GetKernelW()
+	} else {
+		numKernelDims := len(conv.ConvParam.GetKernelSize())
+		if numKernelDims == 1 || numKernelDims == conv.numSpatialAxis {
+			return errors.New("kernel_size must be specified once, or once per spatial dimension.")
+		}
+		for i := 0; i < conv.numSpatialAxis; i++ {
+			if numKernelDims == 1 {
+				conv.kernelShapeData.Data[i] = conv.ConvParam.GetKernelSize()[0]
+			} else {
+				conv.kernelShapeData.Data[i] = conv.ConvParam.GetKernelSize()[i]
+			}
+		}
+	}
+
+	for i = 0; i < conv.numSpatialAxis; i++ {
+		if conv.kernelShapeData[i] <= 0 {
+			return errors.New("Filter dimensions must be nonzeros.")
+		}
+	}
+
+	// setup stride dimensions
+	conv.stride.Reshape(spatialDimBlobShape)
+	if conv.ConvParam.GetStrideH() > 0 || conv.ConvParam.GetStrideW() > 0 {
+		if conv.numSpatialAxis != 2 {
+			return errors.New("stride_h & stride_w can only be used for 2D convolution.")
+		}
+		if len(conv.ConvParam.GetStride()) != 0 {
+			return errors.New("Either stride or stride_h/w should be specified; not both.")
+		}
+		conv.stride.Data[0] = conv.ConvParam.GetStrideH()
+		conv.stride.Data[1] = conv.ConvParam.GetStrideW()
+	} else {
+		numStrideDims := len(conv.ConvParam.GetStride())
+		if numStrideDims == 0 || numStrideDims == 1 || numStrideDims == conv.numSpatialAxis {
+			return errors.New("stride must be specified once, or once per spatical dimension.")
+		}
+		kDefaultStride := 1
+		for i := 0; i < conv.numSpatialAxis; i++ {
+			if numStrideDims == 0 {
+				conv.stride.Data[i] = kDefaultStride
+			} else if numStrideDims == 1 {
+				conv.stride.Data[i] = conv.ConvParam.GetStride()[0]
+			} else {
+				conv.stride.Data[i] = conv.ConvParam.GetStride()[i]
+			}
+			if conv.stride.Data[i] <= 0 {
+				return errors.New("stride dimensions must be nonzero.")
+			}
+		}
+	}
+
+	// setup pad dimensions
+	conv.pad.Reshape(spatialDimBlobShape)
+	if conv.ConvParam.GetPadH() > 0 || conv.ConvParam.GetPadW() > 0 {
+		if conv.numSpatialAxis != 2 {
+			return errors.New("pad_h & pad_w can only be used for 2D convolution.")
+		}
+		if len(conv.ConvParam.GetPad() != 0) {
+			return errors.New("Either pad or pad_h/w should be specified; not both.")
+		}
+		conv.pad.Data[0] = conv.ConvParam.GetPadH()
+		conv.pad.Data[1] = conv.ConvParam.GetPadW()
+	} else {
+		numPadDims := len(conv.ConvParam.GetPad())
+		if numPadDims != 0 && numPadDims != 1 && numPadDims != conv.numSpatialAxis {
+			return errors.New("pad must be specified once, or once per spatial dimension")
+		}
+		kDefaultPad := 0
+		for i := 0; i < conv.numSpatialAxis; i++ {
+			switch numPadDims {
+			case 0:
+				conv.pad.Data[i] = kDefaultPad
+			case 1:
+				conv.pad.Data[i] = conv.ConvParam.GetPad()[0]
+			default:
+				conv.pad.Data[i] = conv.ConvParam.GetPad()[i]
+			}
+		}
+	}
+
+	// setup dilation dimensions
 }
 
 func (conv *ConvLayer) Reshape(bottom, top []*blob.Blob) {
