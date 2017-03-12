a4439b0684336ae518a608792988d6fe98fe5ae7
cvley <ley@hackcv.com>
2017-03-13 07:46:22 +0800

    init split layer

---
+++ layer/split.go
@@ -0,0 +1,35 @@
+package layer
+
+import (
+	"log"
+
+	"github.com/cvley/gocaffe/blob"
+	//pb "github.com/cvley/gocaffe/proto"
+)
+
+type SplitLayer struct {
+	count int
+}
+
+func NewSplitLayer(count int) *SplitLayer {
+	if count <= 0 {
+		panic("new split layer fail, invalid count")
+	}
+
+	return &SplitLayer{count: count}
+}
+
+func (split *SplitLayer) Forward(bottom []*blob.Blob) ([]*blob.Blob, error) {
+	top := make([]*blob.Blob, split.count)
+
+	for i := 0; i < split.count; i++ {
+		top[i] = bottom[0].Copy()
+	}
+
+	return top, nil
+}
+
+// Type of Layer
+func (split *SplitLayer) Type() string {
+	return "SplitLayer"
+}
