ba8d68ddd5231ad463435e7d6a1c06e7f0ff2b9b
cvley <ley@hackcv.com>
2017-03-13 15:33:44 +0800

    add eltwise.go

---
+++ layer/eltwise.go
@@ -0,0 +1,138 @@
+package layer
+
+import (
+	"errors"
+	"math"
+
+	"github.com/cvley/gocaffe/blob"
+	pb "github.com/cvley/gocaffe/proto"
+)
+
+type EltwiseLayer struct {
+	op            pb.EltwiseParameter_EltwiseOp
+	coeffs        []float64
+	maxIdx        *blob.Blob
+	stableProGrad bool
+}
+
+func NewEltwiseLayer(param *pb.V1LayerParameter) (*EltwiseLayer, error) {
+	eltwiseParam := param.GetEltwiseParam()
+	if eltwiseParam == nil {
+		return nil, errors.New("create eltwise layer fail, invalid parameter")
+	}
+
+	coeff := eltwiseParam.GetCoeff()
+	if coeff == nil {
+		return nil, errors.New("create eltwise layer fail, invalid coeff")
+	}
+
+	coeffs := make([]float64, len(coeff))
+	for i, v := range coeffs {
+		coeffs[i] = float64(v)
+	}
+
+	return &EltwiseLayer{
+		op:            eltwiseParam.GetOperation(),
+		coeffs:        coeffs,
+		stableProGrad: eltwiseParam.GetStableProdGrad(),
+	}, nil
+}
+
+func (elt *EltwiseLayer) Forward(bottom []*blob.Blob) ([]*blob.Blob, error) {
+	switch elt.op {
+	case pb.EltwiseParameter_PROD:
+		top, err := bottom[0].Dot(bottom[1], blob.ToData)
+		if err != nil {
+			return nil, err
+		}
+		for i := 2; i < len(bottom); i++ {
+			top, err = top.Dot(bottom[i], blob.ToData)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		return []*blob.Blob{top}, nil
+
+	case pb.EltwiseParameter_SUM:
+		coeffs, err := blob.New(bottom[0].Shape())
+		if err != nil {
+			return nil, err
+		}
+		for n := 0; n < bottom[0].Num(); n++ {
+			for c := 0; c < bottom[0].Channels(); c++ {
+				for h := 0; h < bottom[0].Height(); h++ {
+					for w := 0; w < bottom[0].Width(); w++ {
+						index := coeffs.Offset([]int{n, c, h, w})
+						coeffs.Set([]int{n, c, h, w}, elt.coeffs[index], blob.ToData)
+					}
+				}
+			}
+		}
+		top := make([]*blob.Blob, len(bottom))
+		for i, v := range bottom {
+			result, err := v.Dot(coeffs, blob.ToData)
+			if err != nil {
+				return nil, err
+			}
+			top[i] = result
+		}
+
+		return top, nil
+
+	case pb.EltwiseParameter_MAX:
+		mask, err := blob.Init(bottom[0].Shape(), -1, blob.ToData)
+		if err != nil {
+			return nil, err
+		}
+		top, err := blob.Init(bottom[0].Shape(), -math.MaxFloat64, blob.ToData)
+		if err != nil {
+			return nil, err
+		}
+		for n := 0; n < bottom[0].Num(); n++ {
+			for c := 0; c < bottom[0].Channels(); c++ {
+				for h := 0; h < bottom[0].Height(); h++ {
+					for w := 0; w < bottom[0].Width(); w++ {
+						idxs := []int{n, c, h, w}
+						data0 := bottom[0].Get(idxs, blob.ToData)
+						data1 := bottom[1].Get(idxs, blob.ToData)
+						if data0 > data1 {
+							top.Set(idxs, data0, blob.ToData)
+							mask.Set(idxs, 0, blob.ToData)
+						} else {
+							top.Set(idxs, data1, blob.ToData)
+							mask.Set(idxs, 1, blob.ToData)
+						}
+					}
+				}
+			}
+		}
+
+		for i := 2; i < len(bottom); i++ {
+			for n := 0; n < bottom[0].Num(); n++ {
+				for c := 0; c < bottom[0].Channels(); c++ {
+					for h := 0; h < bottom[0].Height(); h++ {
+						for w := 0; w < bottom[0].Width(); w++ {
+							idxs := []int{n, c, h, w}
+							data0 := bottom[i].Get(idxs, blob.ToData)
+							data1 := top.Get(idxs, blob.ToData)
+							if data0 > data1 {
+								top.Set(idxs, data0, blob.ToData)
+								mask.Set(idxs, float64(i), blob.ToData)
+							}
+						}
+					}
+				}
+			}
+		}
+
+		elt.maxIdx = mask
+		return []*blob.Blob{top}, nil
+	}
+
+	return nil, errors.New("Unknown elementwise operation")
+}
+
+func (elt *EltwiseLayer) Type() string {
+	return "Eltwise"
+}
