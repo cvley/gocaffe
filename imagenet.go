cfb1658e2eb98e095306c1edfb5c2513ca380015
cvley <ley@hackcv.com>
2017-07-13 21:12:20 +0800

    add imagenet binary

---
+++ imagenet.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/cvley/gocaffe/blob"
+	"github.com/cvley/gocaffe/io"
+	"github.com/cvley/gocaffe/net"
+)
+
+func main() {
+	deploy := flag.String("deploy", "", "deploy prototxt file")
+	model := flag.String("model", "", "trained model")
+	image := flag.String("image", "", "input image")
+	flag.Parse()
+
+	if *deploy == "" || *model == "" {
+		log.Println("invalid deploy or model file")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if *image == "" {
+		log.Println("invalid image file")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	f, err := os.Open(*deploy)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	n, err := net.New(string(b))
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	if err := n.CopyTrainedLayersFromFile(*model); err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	height, width := n.GetInputSize()
+	bottom, err := io.ReadImageFile(*image, int(width), int(height))
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	log.Println(bottom.Shape())
+	tops, err := n.Forward([]*blob.Blob{bottom})
+	if err != nil {
+		log.Println("ERROR Forward", err)
+		os.Exit(1)
+	}
+
+//	for i:=0; i< 1000; i++ {
+		log.Printf("%f ", tops[0].Get([]int{0, 0, 0, 0}))
+//	}
+}
